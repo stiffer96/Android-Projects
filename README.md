d07f7f14a0d902e3a6c85738115fad326bb305f4
Sourav <souravchoudhary720@gmail.com>
2021-08-27 15:50:37 +0530

    Zero Cross game is created the first game of my Android Learning

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Android-Projects
+
+
++-------------------------------------------------------------------------------------------------------------+
+Day Friday : 27-Aug-2021
+
+	--> Project : Zero Cross Game
+			--> Added project here
+			--> In this project i have created a zero cross game which we all are playing since childhood
+			--> Used Design protion : Relative Layour [ Grid Layout [3,3] - for game area ,
+					 Linear vertical Layout - for showing winning or draw message and play again button ]
+			-->Activity - MainActivity - dropIn() method - to add animation on every touch in play area and
+											full functioning of game.
+									   - playAgain() method - to reset everything .	
+
+git commit -m "Zero Cross game is created the first game of my Android Learning"
++-------------------------------------------------------------------------------------------------------------+
---
+++ ZeroCross/app/src/main/java/com/example/zerocross/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.zerocross;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import androidx.gridlayout.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //0 - player 1 cross , 1 - player 2 zero
+    private int activePlayer = 0;
+
+    private boolean gameIsActive = true;
+
+    private int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    private int[][] winComb = {{0,1,2},{3,4,5},{6,7,8},{0,3,6,},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+    public void dropIn(View view){
+        ImageView field = (ImageView) view;
+
+        int tag = Integer.parseInt(field.getTag().toString());
+
+        if(gameState[tag] == 2 && gameIsActive){
+
+            gameState[tag] = activePlayer;
+
+            field.setTranslationY(-1000f);
+
+            if(activePlayer == 0){
+                field.setImageResource(R.drawable.cross);
+                activePlayer = 1;
+            }
+            else{
+                field.setImageResource(R.drawable.zero);
+                activePlayer = 0;
+            }
+
+            field.animate().translationYBy(1000f).setDuration(300);
+
+            for(int[] winningPositions : winComb){
+                if(gameState[winningPositions[0]] == gameState[winningPositions[1]]
+                        && gameState[winningPositions[1]] == gameState[winningPositions[2]]
+                        && gameState[winningPositions[0]] != 2 ){
+
+                    //when someone win
+                    gameIsActive=false;
+
+                    String player = "Cross";
+                    if(gameState[winningPositions[0]] == 1) player ="Zero";
+
+                    TextView textView = findViewById(R.id.winMsg);
+                    textView.setText(player+" win");
+                    LinearLayout layout = findViewById(R.id.linearLayout);
+                    layout.setVisibility(View.VISIBLE);
+
+                }
+                else{
+                    boolean gameIsOver = true;
+                    for(int fieldState : gameState){
+                        if(fieldState == 2) gameIsOver = false;
+                    }
+
+                    if(gameIsOver){
+                        TextView textView = findViewById(R.id.winMsg);
+                        textView.setText("It's a Tie ");
+                        LinearLayout layout = findViewById(R.id.linearLayout);
+                        layout.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+        }
+
+
+    }
+
+    public void  playAgain(View view){
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+        layout.setVisibility(View.INVISIBLE);
+
+        //setting game state to default
+        for(int i = 0 ; i < gameState.length ; i ++){
+            gameState[i] = 2;
+        }
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for(int i= 0 ; i < gridLayout.getChildCount() ; i ++){
+            ((ImageView)gridLayout.getChildAt(i)).setImageResource(0);
+        }
+
+        gameIsActive=true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
